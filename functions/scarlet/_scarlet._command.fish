64b41c9553bfaa87904b1fd5d13669a1e33f62d8
A.M <andares@outlook.com>
2019-12-04 19:43:39 +0800

    fix bu

--- functions/scarlet/_scarlet._command.fish
+++ functions/scarlet/_scarlet._command.fish
@@ -1,6 +1,6 @@
 function _scarlet._command
   set -l commands setup bbbb
-  if test argv[1]
+  if test (count $argv) -gt 0
     for command in $commands
       if test $name = $command
         echo $name
