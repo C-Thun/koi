b39ca09d4fb00e1dd8b3eb3e0564e439c0c6582f
A.M <andares@outlook.com>
2019-12-04 19:59:12 +0800

    update

--- functions/scarlet/_scarlet.setup.fish
+++ functions/scarlet/_scarlet.setup.fish
@@ -2,7 +2,7 @@ function _scarlet.setup -a name
   set -l commands setup
   if test -n "$name"
     for command in $commands
-      if test $name = $command
+      if test "$name" = "$command"
         echo $name
         break
       end
