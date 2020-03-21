5d1947a7bce675865838312c2eb267da96e3c52a
A.M <andares@outlook.com>
2020-03-22 04:33:18 +0800

    update

--- functions/koi/_koi.help.fish
+++ functions/koi/_koi.help.fish
@@ -1,6 +1,7 @@
 function _koi.help -d 'show manual' -a url
-  set -l specs 帮助信息 更新KLib库 注册/取消域名指向 远程安装deb 拉取远程文件 重置仓库 放弃仓库当前未提交变更 开关nginx站点配置
-  for var in (_koi._command)$specs
-    echo $var
+  set -l specs \t帮助信息 更新KLib库 注册/取消域名指向 远程安装deb 拉取远程文件 重置仓库 放弃仓库当前未提交变更 开关nginx站点配置
+  set -l commands (string split ' ' (_koi._command))
+  for i in (seq (count $commands))
+    echo - $commands[$i]\t\t\t$specs[$i]
   end
 end
