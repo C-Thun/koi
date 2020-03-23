b31888201ec0935dba7137f9e11fc264a64023d3
A.M <andares@outlook.com>
2020-03-23 20:46:39 +0800

    update

--- functions/koi/_koi.exec-cmd.fish
+++ functions/koi/_koi.exec-cmd.fish
@@ -3,9 +3,11 @@ function _koi.exec-cmd -a cmd need_root
     set cmd sudo $cmd
   end
 
-  echo "function ___last_command --no-scope-shadowing
-    $cmd
-  end"
+  # echo "function ___last_command --no-scope-shadowing
+  #   $cmd
+  # end"
+
+  echo ">>> exec cmd: $cmd"
 
   echo "function ___last_command --no-scope-shadowing
     $cmd
