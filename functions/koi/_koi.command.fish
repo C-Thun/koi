a2c1ea072304a6b5333f5425e606c91bb95476c5
A.M <andares@outlook.com>
2020-03-23 16:45:13 +0800

    update

--- functions/koi/_koi.command.fish
+++ functions/koi/_koi.command.fish
@@ -2,12 +2,12 @@ function _koi.command -a name
   set -l commands
   for func in (functions -a)
     set -l tmp (string match -r '^_koi\.(.+)' $func)
-    if $tmp
+    if count $tmp
       set commands $commands $tmp[2]
     end
   end
 
-  if $argv
+  if $name
     for command in $commands
       if test "$name" = "$command"
         echo $command
