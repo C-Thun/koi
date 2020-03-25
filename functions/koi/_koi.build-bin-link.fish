9fe1b55b915e288243bd14506b0ceefec76b424a
A.M <andares@outlook.com>
2020-03-25 19:49:45 +0800

    update

--- functions/koi/_koi.build-bin-link.fish
+++ functions/koi/_koi.build-bin-link.fish
@@ -1,6 +1,6 @@
 function _koi.build-bin-link -a dir
   # 遍历目录，把缺失的软链补上
-  if test (count ls $dir) -gt 2
+  if not test -z $dir; and test (count ls $dir) -gt 2
     for file in (string split ' ' (echo $dir/*))
       chmod +x $file
 
