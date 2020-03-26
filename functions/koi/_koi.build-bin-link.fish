a6278639d39ac76e8968b7bd5f9d81f8c1a327ff
A.M <andares@outlook.com>
2020-03-26 15:17:48 +0800

    update

--- functions/koi/_koi.build-bin-link.fish
+++ functions/koi/_koi.build-bin-link.fish
@@ -1,12 +1,11 @@
 function _koi.build-bin-link -a dir
   if not test -z $dir; and test (count (string split ' ' (ls $dir))) -gt 0
     for file in (string split ' ' (echo $dir/*))
-      # chmod +x $file
+      chmod +x $file
 
       set -l bin_name (basename (echo $file | sed 's/\.[^.]*$//'))
       if not test -e ~/local/bin/$bin_name
         ln -s $file ~/local/bin/$bin_name
-        chmod +x ~/local/bin/$bin_name
       end
     end
   end
