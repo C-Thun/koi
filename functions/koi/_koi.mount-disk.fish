ac2134c4768f12aa74013756bf814d8b50c99d67
A.M <andares@outlook.com>
2020-04-10 16:24:59 +0800

    update

--- functions/koi/_koi.mount-disk.fish
+++ functions/koi/_koi.mount-disk.fish
@@ -1,8 +1,29 @@
-function _koi.mount-disk -a dev target
+# koi mount-disk vdb1 /opt/data01 ashe
+function _koi.mount-disk -a dev target user
   if test -z "$dev"; or test -z "$target"
-    echo Usage: koi mount-disk \<dev\> \<target\>
+    echo Usage: koi mount-disk \<dev\> \<target\>[ \<user\>]
     return $OMF_UNKNOWN_OPT
   end
 
-  koi exec-cmd "blkid /dev/$dev" y
+  if test -z "$user"
+    set user $USER
+  end
+
+  # 创建目录
+  if not test -d $target
+    koi exec-cmd "mkdir -p $target" y
+    koi exec-cmd "chown $user:$user $target" y
+  end
+
+  set -l tmp (string match -r '^[^ :]+: UUID="([0-9a-z\-]+)" TYPE="([0-9a-zA-Z\-]+)"' (koi exec-cmd "blkid /dev/$dev" y))
+  set -l blkid $tmp[2]
+  set -l ftype $tmp[3]
+
+  koi exec-cmd "cp -f /etc/fstab /etc/fstab.backup" y
+  koi exec-cmd "sed -i '/^UUID=$blkid\t/d' /etc/fstab" y
+  koi exec-cmd "sed -i '\$a\\UUID=$blkid\t$target\t$ftype\tdefaults\t0\t2' /etc/fstab" y
+
+  # cat /etc/fstab
+  koi exec-cmd "mount -a" y
+  koi exec-cmd "mount | grep $target" y
 end
