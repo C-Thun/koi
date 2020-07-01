9934356a890c59e551a56c7afd047ed3a7b1a79b
A.M <andares@outlook.com>
2020-07-01 18:23:53 +0800

    update

--- functions/__koi_seen_--.fish
+++ functions/__koi_seen_--.fish
@@ -1,15 +1,9 @@
-function __koi_seen_subcommand_from
+function __koi_seen_--
     set -l cmd (commandline -poc)
     set -e cmd[1]
 
     for i in $cmd
         if contains -- $i "--"
-            return 1
-        end
-    end
-
-    for i in $cmd
-        if contains -- $i $argv
             return 0
         end
     end
