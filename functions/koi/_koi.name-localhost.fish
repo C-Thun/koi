7c21f59d27ed7240417259966f5c527b354d4c4b
A.M <andares@outlook.com>
2020-07-02 17:34:47 +0800

    add usage info to koi name-localhost

--- functions/koi/_koi.name-localhost.fish
+++ functions/koi/_koi.name-localhost.fish
@@ -1,4 +1,9 @@
 function _koi.name-localhost -a name
+  if test -z "$name"
+    echo Usage: koi name-localhost \<name\>
+    return 0
+  end
+
   koi exec-cmd "hostname $name" y
   if test -f /etc/hostname
     koi exec-cmd "echo $name > /etc/hostname" y
