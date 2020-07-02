9d989b1c01029c7b8a91b30c6514a935b03019e9
A.M <andares@outlook.com>
2020-07-02 17:11:33 +0800

    update

--- functions/koi/_koi.register-host.fish
+++ functions/koi/_koi.register-host.fish
@@ -2,13 +2,16 @@ function _koi.register-host -a domain ip apply
   set -l cmd sed
   if not test -z "$apply"
     set cmd $cmd -i
+    return 0
   end
 
-  if test "$ip" = "0"
-    koi exec-cmd "$cmd '/\t$domain\t\$/d' /etc/hosts" y
-  else
-    koi exec-cmd "$cmd '/\t$domain\t\$/d' /etc/hosts" y
-    koi exec-cmd "$cmd '\$a\\$ip\t$domain\t' /etc/hosts" y
+  if not test -z "$domain"; and not test -z "$ip"
+    if test "$ip" = "0"
+      koi exec-cmd "$cmd '/\t$domain\t\$/d' /etc/hosts" y
+    else
+      koi exec-cmd "$cmd '/\t$domain\t\$/d' /etc/hosts" y
+      koi exec-cmd "$cmd '\$a\\$ip\t$domain\t' /etc/hosts" y
+    end
   end
 
   if not test -z "$apply"
