9474f0c464dca27f9e77eb5667fcf4c0b2a54aad
A.M <andares@outlook.com>
2020-07-03 12:45:40 +0800

    update

--- functions/koi/_koi.debug.fish
+++ functions/koi/_koi.debug.fish
@@ -1,9 +1,15 @@
-function _koi.debug -d "Enable/Disable \$DEBUG var"
-  if test -z "$DEBUG"
-    set -xg DEBUG 1
-    echo DEBUG is Enabled
-  else
-    set -xg DEBUG
-    echo DEBUG is Disabled
+function _koi.debug -a msg -d "Enable/Disable \$DEBUG var"
+  if test -z "$msg"
+    if test -z "$DEBUG"
+      set -xg DEBUG 1
+      echo DEBUG is Enabled
+    else
+      set -xg DEBUG
+      echo DEBUG is Disabled
+    end
+  end
+
+  if not test -z "$DEBUG"
+    echo $msg
   end
 end
