6ce7ea965089bfca0b35f9b1b7e31ba1c16f0731
A.M <andares@outlook.com>
2020-07-16 17:08:38 +0800

    update

--- functions/koi/_koi.kc-mass.fish
+++ functions/koi/_koi.kc-mass.fish
@@ -7,5 +7,5 @@ function _koi.kc-mass -a cmd kind name namespace
   if not test -z "$namespace"
     set namespace -n $namespace
   end
-  echo kubectl get $kind $namespace \| grep $name \| awk '{print $1}' \| xargs kubectl $cmd $kind $namespace $argv[5..-1]
+  kubectl get $kind $namespace | grep $name | awk '{print $1}' | xargs kubectl $cmd $kind $namespace $argv[5..-1]
 end
