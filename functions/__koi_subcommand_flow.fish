3158eeb2460739c4b4e67aa2891ddb188a5c1a09
A.M <andares@outlook.com>
2020-08-06 19:35:35 +0800

    update

--- functions/__koi_subcommand_flow.fish
+++ functions/__koi_subcommand_flow.fish
@@ -2,10 +2,23 @@ function __koi_subcommand_flow
     set -l cmd (commandline -poc)
     set -e cmd[1]
 
+    set -l check_cmd
     for i in $argv
-        if test $cmd[$i] -ne $argv[$i]
+        # argv 未走完但cmd已经没有了
+        if test (count $cmd) -eq 0
             return 0
         end
+
+        if test (count $cmd) -eq 1
+            set check_cmd $cmd
+        else
+            set check_cmd $cmd[1]
+        end
+        echo test $check_cmd -ne $i >> ~/debug.log
+        if test "$check_cmd" -ne "$i"
+            return 0
+        end
+        set -e cmd[1]
     end
 
     return 1
