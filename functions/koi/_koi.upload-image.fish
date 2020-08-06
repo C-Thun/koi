61b504dd66e77aa18ffe9ba0bfb1543adbceef8d
A.M <andares@outlook.com>
2020-08-06 18:14:09 +0800

    updte

--- functions/koi/_koi.upload-image.fish
+++
@@ -1,6 +0,0 @@
-function _koi.upload-image -d "Upload your image" -a major minor
-  if test -z "$inventory"; or test -z "$books"
-    echo Usage: play \<inventory\> \<...books\> [-- \<...extra\>]
-    return $OMF_UNKNOWN_OPT
-  end
-end
