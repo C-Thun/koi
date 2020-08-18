b5aff5c9fd98a77740c5adf25ac76b0302101077
Andares Merigold <andares@outlook.com>
2020-08-18 16:34:36 +0800

    update

--- functions/hooks/hook_clear-history.fish
+++ functions/hooks/hook_clear-history.fish
@@ -1,4 +1,4 @@
-function _hook_clear-history --on-process-exit %self
+function hook_clear-history --on-process-exit %self
   if test $autoclear_history -gt 0
     builtin history clear
     echo Session history scrubbed.  Goodbye
