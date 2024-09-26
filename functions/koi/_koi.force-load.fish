4544f0bcc2cbd53648c1b8c0111ab7d156360b37
Andares Mer <andares@foxmail.com>
2024-09-27 00:24:49 +0800

    update

--- functions/koi/_koi.force-load.fish
+++ functions/koi/_koi.force-load.fish
@@ -1,7 +1,9 @@
 function _koi.force-load -a dir
   if test (count (string split ' ' (ls $dir))) -gt 0
     for file in (string split ' ' (echo $dir/*))
-      source $file
+      if not test -d $file
+        source $file
+      end
     end
   end
 end
