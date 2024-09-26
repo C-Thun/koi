529cbd044488b7ec1c18a57619dea8173ac8af13
Andares Mer <andares@foxmail.com>
2024-09-26 14:58:25 +0800

    updt

--- functions/koi/_koi.set-rc.fish
+++ functions/koi/_koi.set-rc.fish
@@ -5,7 +5,7 @@ function _koi.set-rc -a name value
   end
 
   if not test -f $HOME/.koirc.fish
-    echo '# KOI Persistent Environment Variables' > $HOME/.koirc.fish
+    echo '# KOI RC Environment Variables' > $HOME/.koirc.fish
   end
 
   koi exec-cmd "sed -i '/^set -xg $name $value\$/d' $HOME/.koirc.fish"
