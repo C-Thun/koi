b0c96bb590b4398378f227bb6ae1b2819b084a65
Andares Mer <andares@foxmail.com>
2024-09-26 14:50:54 +0800

    fix bug for set-rc

--- functions/koi/_koi.force-load.fish
+++ functions/koi/_koi.force-load.fish
@@ -1,15 +1,7 @@
 function _koi.force-load -a dir
-  if test -z "$name"; or test -z "$value"
-    echo Usage: koi set-rc \<name\> \<value\>
-    return $OMF_UNKNOWN_OPT
+  if test (count (string split ' ' (ls $dir))) -gt 0
+    for file in (string split ' ' (echo $dir/*))
+      echo $file
+    end
   end
-
-  if not test -f $HOME/.koirc.fish
-    echo '# KOI Persistent Environment Variables' > $HOME/.koirc.fish
-  end
-
-  koi exec-cmd "sed -i '/^set -xg $name $value\$/d' $HOME/.koirc.fish"
-  koi exec-cmd "sed -i '\$a\\set -xg $name $value' $HOME/.koirc.fish"
-
-  set -xg "$name" "$value"
 end
